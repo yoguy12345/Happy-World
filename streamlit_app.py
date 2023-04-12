6871ccfbc04df5d4854d67173ed4fec20be6263b
yoguy12345 <52938370+yoguy12345@users.noreply.github.com>
2023-04-12 02:13:18 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ else:
 # with open('my_datalist.pickle', 'rb') as f:
 #     datalist = pickle.load(f)
 
-print(datalist[1].columns)
+# print(datalist[1].columns)
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
